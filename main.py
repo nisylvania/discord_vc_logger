a8afad32db7d291a3d6d0bbe56e96387c60c616f
naohiro <nao114computer@gmail.com>
2021-07-30 00:56:11 +0900

    Delete Description Fix color

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
         embed = discord.Embed(
             title="入室通知",
-            color=0x00ff00,
-            description="<#" + str(after.channel.id) + ">に<@!" + str(member.id) + ">が参加しました。"
+            color=member.color
         )
         embed.set_thumbnail(url=member.avatar_url)
         embed.add_field(name="参加チャンネル", value="<#" + str(after.channel.id) + ">")
