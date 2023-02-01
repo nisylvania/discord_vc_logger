2a0c690c5b62ead59b59575143676aa182aded9b
kzmw <kazumanombiri@icloud.com>
2023-02-01 15:20:41 +0900

    sub-account

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, Intents, MessageEmbed } = require('discord.js')
 const client = new Client({ intents: Object.keys(Intents.FLAGS) })
-const version = {text : 'Version 1.0'};
 require('dotenv').config();
 
 const send_vc_notify = (voicestate, is_exit) => {
@@ -9,26 +8,30 @@ const send_vc_notify = (voicestate, is_exit) => {
     const ChannelName = voicestate.channel.name;
     const displayAvatarURL = voicestate.member.displayAvatarURL();
     const ChannelMemberSize = voicestate.channel.members.size;
-    const Embed = new MessageEmbed()
+    const Embed = new MessageEmbed();
+    const date_b = new Date()
+    const date = date_b.toLocaleString();
+
+
     Embed.setColor(displayColor);
-        if (is_exit == false){
-            let author= {
-                name: "VC入室",
-                iconURL: displayAvatarURL
-            }
-            Embed.setTitle(displayName + "が" + ChannelName + "に入室しました！");
-            Embed.setAuthor(author);
+    if (is_exit == false) {
+        let author = {
+            name: "VC入室",
+            iconURL: displayAvatarURL
         }
-        else if (is_exit == true){
-            let author= {
-                name: "VC退室",
-                iconURL: displayAvatarURL
-            }
-            Embed.setTitle(displayName + "が" + ChannelName + "から退室しました！");
-            Embed.setAuthor(author);
+        Embed.setTitle(displayName + "が" + ChannelName + "に入室しました");
+        Embed.setAuthor(author);
+    }
+    else if (is_exit == true) {
+        let author = {
+            name: "VC退室",
+            iconURL: displayAvatarURL
         }
-        Embed.setDescription("現在の参加者数は" + String(ChannelMemberSize) + "人です。")
-        Embed.setFooter(version)
+        Embed.setTitle(displayName + "が" + ChannelName + "から退室しました");
+        Embed.setAuthor(author);
+    }
+    Embed.setDescription("現在の参加者数は" + String(ChannelMemberSize) + "人です")
+    Embed.setFooter({text: date})
     voicestate.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
 }
 
@@ -36,24 +39,22 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     if (newState && oldState) {
         if (oldState.channelId === null && newState.channelId != null) {
             const roles = newState.member.roles.cache;
+            const roles2 = roles.map((role) => (role.name));
+            const members = newState.channel.members;
+            const members2 = members.map((member) => (member.user.tag));
             const is_bot = newState.member.user.bot;
-            if (!is_bot) {
-                roles.map((role) => {
-                    if (role.name == "VC Entry") {
-                        send_vc_notify(newState, false);
-                    }
-                });
+            if (!is_bot && roles2.includes('VC Entry') && roles2.filter(item => roles2.includes(item) && members2.includes(item)).length <= 0) {
+                send_vc_notify(newState, false);
             }
         }
         else if (oldState.channelId != null && newState.channelId === null) {
             const roles = oldState.member.roles.cache;
+            const roles2 = roles.map((role) => (role.name));
+            const members = oldState.channel.members;
+            const members2 = members.map((member) => (member.user.tag));
             const is_bot = oldState.member.user.bot;
-            if (!is_bot) {
-                roles.map((role) => {
-                    if (role.name == "VC Exit") {
-                        send_vc_notify(oldState, true);
-                    }
-                });
+            if (!is_bot && roles2.includes('VC Exit') && roles2.filter(item => roles2.includes(item) && members2.includes(item)).length <= 0) {
+                send_vc_notify(oldState, true);
             }
         }
     }
