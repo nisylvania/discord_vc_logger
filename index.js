ff37d21231e8bdd4a53e5e5dd6fd37d1fbc1f49c
kzmw <kazumanombiri@icloud.com>
2023-05-01 01:00:54 +0900

    streaming

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const send_vc_notify = (voicestate, is_exit) => {
     const ChannelMemberSize = voicestate.channel.members.size;
     const Embed = new MessageEmbed();
     const date = moment(new Date()).format('Y/M/D H:mm:ss');
-    
+
 
 
     Embed.setColor(displayColor);
@@ -32,12 +32,32 @@ const send_vc_notify = (voicestate, is_exit) => {
         Embed.setAuthor(author);
     }
     Embed.setDescription("現在の参加者数は" + String(ChannelMemberSize) + "人です")
-    Embed.setFooter({text: date})
+    Embed.setFooter({ text: date })
+    voicestate.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
+}
+
+const send_st_notify = (voicestate) => {
+    const displayColor = voicestate.member.displayColor;
+    const displayName = voicestate.member.displayName;
+    const ChannelName = voicestate.channel.name;
+    const displayAvatarURL = voicestate.member.displayAvatarURL();
+    const Embed = new MessageEmbed();
+    const date = moment(new Date()).format('Y/M/D H:mm:ss');
+
+    Embed.setColor(displayColor);
+    let author = {
+        name: "VC画面共有",
+        iconURL: displayAvatarURL
+    }
+    Embed.setTitle(displayName + "が" + ChannelName + "で画面共有を開始しました");
+    Embed.setAuthor(author);
+    Embed.setFooter({ text: date })
     voicestate.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
 }
 
 client.on('voiceStateUpdate', (oldState, newState) => {
     if (newState && oldState) {
+
         if (oldState.channelId === null && newState.channelId != null) {
             const roles = newState.member.roles.cache;
             const roles2 = roles.map((role) => (role.name));
@@ -56,6 +76,16 @@ client.on('voiceStateUpdate', (oldState, newState) => {
             const is_bot = oldState.member.user.bot;
             if (!is_bot && roles2.includes('VC Exit') && roles2.filter(item => roles2.includes(item) && members2.includes(item)).length <= 0) {
                 send_vc_notify(oldState, true);
+            }
+        }
+        else if (oldState.channelId != null && newState.channelId != null && oldState.streaming == false && newState.streaming == true) {
+            const roles = newState.member.roles.cache;
+            const roles2 = roles.map((role) => (role.name));
+            const members = newState.channel.members;
+            const members2 = members.map((member) => (member.user.tag));
+            const is_bot = newState.member.user.bot;
+            if (!is_bot && (roles2.includes('VC Entry') || roles2.includes('VC Exit')) && roles2.filter(item => roles2.includes(item) && members2.includes(item)).length <= 0) {
+                send_st_notify(newState);
             }
         }
     }
